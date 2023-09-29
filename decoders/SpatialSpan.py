d1b6d68c09c8fc18ac047e201bea7acdcd61276d
Kelvin O. Lim <kolim@umn.edu>
2023-09-29 11:50:10 -0500

    add task decoders modules

---
+++ decoders/SpatialSpan.py
@@ -0,0 +1,7 @@
+# function that accepts the json data and returns the result in a dict
+
+# for spatial scan
+def decode(jsonData):
+    
+    
+    return {"spatialScan_raw": 5}
---
+++ decoders/TrailsAB.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+import statistics
+import json
+
+
+# function that accepts the json data and returns the result in a dict
+
+
+def decode(gridData,label='Grid'):
+    
+    # check that there is data
+    if type(gridData) is list:
+        # just get the trial_types we want
+        grid_dict = [x for x in gridData if x['trial_type'] == 'trails']
+
+        # get time_elapsed for each trailsType
+        
+        results = {}
+        for item in grid_dict:
+            key = f"{label}{item['trailsType']}_secs"
+            value = item['time_elapsed']
+            # convert to seconds float
+            value = value/1000.0
+            results[key]=value
+    else:
+        # empty results
+        results = {
+            f"{label}A_secs": None,
+            f"{label}B_secs": None,            
+        }
+        
+    return results    
+        
+
+
